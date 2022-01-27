99919583a0c67455b219fe7f3c342fc1d034cae2
vismitap <vismita.prabhu@gmail.com>
2022-01-28 01:04:01 +0530

    [Vismita] Add: UC2 solved

--- LineComparison/LineComputation.cs
+++ LineComparison/LineComputation.cs
@@ -10,6 +10,7 @@ namespace LineComparison
     {
         public void ComputeLines()
         {
+            Console.WriteLine("----- Line1 Details -----");
             Console.WriteLine("Enter the (x1,y1) co-ordinates: ");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
@@ -18,8 +19,27 @@ namespace LineComparison
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("The length of the line is: "+length);
+            Console.WriteLine("----- Line2 Details -----");
+            Console.WriteLine("Enter the (x3,y3) co-ordinates: ");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the (x4,y4) co-ordinates: ");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            double firstLineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double secondLineLength = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+
+            if(firstLineLength == secondLineLength)
+            {
+                Console.WriteLine("Line1 and Line2 are EQUAL!");
+            }
+            else
+            {
+                Console.WriteLine("Line1 and Line2 are UNEQUAL!");
+            }
+
         }
     }
 }
