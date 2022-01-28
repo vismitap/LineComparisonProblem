75acc8310c07af02c787038d13cb79653267eb84
vismitap <vismita.prabhu@gmail.com>
2022-01-29 01:16:50 +0530

    [Vismita] Modify: Optimization2 done using constructor

--- LineComparison/LineComputation.cs
+++ LineComparison/LineComputation.cs
@@ -8,55 +8,20 @@ namespace LineComparison
 {
     public class LineComputation
     {
-        public int ReadCoordinateX()
+        int x1, x2, y1, y2;
+        public LineComputation(int x1, int x2, int y1, int y2)
         {
-            int x;
-            return x = Convert.ToInt32(Console.ReadLine());
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
         }
-
-        public int ReadCoordinateY()
-        {
-            int y;
-            return y = Convert.ToInt32(Console.ReadLine());
-        }
-
-        public double CalcLength(int x1, int x2, int y1, int y2)
+       
+        public double CalcLength()
         {
             double length;
-           return length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+           return length = Math.Sqrt(Math.Pow((this.x2 - this.x1), 2) + Math.Pow((this.y2 - this.y1), 2));
         }
-        public void ComputeLines()
-        {
-            Console.WriteLine("----- Line1 Details -----");
-            Console.WriteLine("Enter the (x1,y1) co-ordinates: ");
-            int x1 = ReadCoordinateX(); int y1 = ReadCoordinateY();
 
-            Console.WriteLine("Enter the (x2,y2) co-ordinates: ");
-            int x2 = ReadCoordinateX(); int y2 = ReadCoordinateY();
-
-            Console.WriteLine("----- Line2 Details -----");
-            Console.WriteLine("Enter the (x3,y3) co-ordinates: ");
-            int x3 = ReadCoordinateX(); int y3 = ReadCoordinateY();
-
-            Console.WriteLine("Enter the (x4,y4) co-ordinates: ");
-            int x4 = ReadCoordinateX(); int y4 = ReadCoordinateY();
-
-            double firstLineLength = CalcLength(x1, x2, y1, y2);
-            double secondLineLength = CalcLength(x3, x4, y3, y4);
-
-            if(firstLineLength == secondLineLength)
-            {
-                Console.WriteLine($"Line1 having length = {firstLineLength} and Line2 having length = {secondLineLength} are EQUAL !");
-            }
-            if(firstLineLength > secondLineLength)
-            {
-                Console.WriteLine($"Line1 having length = {firstLineLength} is greater than Line2 having length = {secondLineLength} !");
-            }
-            if(firstLineLength < secondLineLength)
-            {
-                Console.WriteLine($"Line1 having length = {firstLineLength} is lesser than Line2 having length = {secondLineLength} !");
-            }
-
-        }
     }
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -10,9 +10,24 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("************Welcome to Line Comparison Problem!************");
-            LineComputation line = new LineComputation();
-            line.ComputeLines();
+            Console.WriteLine("************Welcome to Line Comparison Problem!************\n");
+            LineComputation line1 = new LineComputation(1, 2, 3, 4);
+            double firstLineLength = line1.CalcLength();
+            LineComputation line2 = new LineComputation(4, 2, 7, 4);
+            double secondLineLength = line2.CalcLength();
+
+            if (firstLineLength.Equals(secondLineLength))
+            {
+                Console.WriteLine($"Line1 having length = {firstLineLength} and Line2 having length = {secondLineLength} are EQUAL. \n");
+            }
+            if (firstLineLength.CompareTo(secondLineLength) > 0)
+            {
+                Console.WriteLine($"Line1 having length = {firstLineLength} is GREATER than Line2 having length = {secondLineLength}. \n");
+            }
+            else
+            {
+                Console.WriteLine($"Line1 having length = {firstLineLength} is LESSER than Line2 having length = {secondLineLength}. \n");
+            }
         }
     }
 }
