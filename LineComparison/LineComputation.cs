9757e3f43083189e00c27451f3bd70c312bea31e
vismitap <vismita.prabhu@gmail.com>
2022-01-29 00:47:03 +0530

    [Vismita] Modify: Optimisation1 done using methods

--- LineComparison/LineComputation.cs
+++ LineComparison/LineComputation.cs
@@ -8,28 +8,41 @@ namespace LineComparison
 {
     public class LineComputation
     {
+        public int ReadCoordinateX()
+        {
+            int x;
+            return x = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public int ReadCoordinateY()
+        {
+            int y;
+            return y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public double CalcLength(int x1, int x2, int y1, int y2)
+        {
+            double length;
+           return length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
         public void ComputeLines()
         {
             Console.WriteLine("----- Line1 Details -----");
             Console.WriteLine("Enter the (x1,y1) co-ordinates: ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x1 = ReadCoordinateX(); int y1 = ReadCoordinateY();
 
             Console.WriteLine("Enter the (x2,y2) co-ordinates: ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            int x2 = ReadCoordinateX(); int y2 = ReadCoordinateY();
 
             Console.WriteLine("----- Line2 Details -----");
             Console.WriteLine("Enter the (x3,y3) co-ordinates: ");
-            int x3 = Convert.ToInt32(Console.ReadLine());
-            int y3 = Convert.ToInt32(Console.ReadLine());
+            int x3 = ReadCoordinateX(); int y3 = ReadCoordinateY();
 
             Console.WriteLine("Enter the (x4,y4) co-ordinates: ");
-            int x4 = Convert.ToInt32(Console.ReadLine());
-            int y4 = Convert.ToInt32(Console.ReadLine());
+            int x4 = ReadCoordinateX(); int y4 = ReadCoordinateY();
 
-            double firstLineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            double secondLineLength = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            double firstLineLength = CalcLength(x1, x2, y1, y2);
+            double secondLineLength = CalcLength(x3, x4, y3, y4);
 
             if(firstLineLength == secondLineLength)
             {
